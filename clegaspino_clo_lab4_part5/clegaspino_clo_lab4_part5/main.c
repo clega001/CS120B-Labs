1f6bfef86794f3cb9054739b897dd44be28a3345
clega001 <clega001@ucr.edu>
2019-01-20 21:52:14 -0800

    fixed lab4_part5 - clo

--- clegaspino_clo_lab4_part5/clegaspino_clo_lab4_part5/main.c
+++ clegaspino_clo_lab4_part5/clegaspino_clo_lab4_part5/main.c
@@ -3,133 +3,139 @@
  * Carissa Lo & clo020@ucr.edu
  * Lab Section: 23
  * Assignment: Lab 4 Exercise 5
- * 
+ *
  * I acknowledge all content contained herein, excluding template or example
- * code, is my own original work.
+ * code is my own original work.
  */ 
+ 
 
 #include <avr/io.h>
-//#include "RIMS.h"
 
-enum States{START, INIT, LOCK, PRESS, UNLOCK, PRESS_UNLOCK} state;
-unsigned char arr[4] = {0x04, 0x01, 0x02, 0x01};
-unsigned char cnt = 0;
-//unsigned char yes = 0;
+enum States {Start, Init, Lock, Press, Unlock, Unlock_code} state;
+
+char arr[4] = {0, 0, 0, 0};
+char arr2[4] = {0, 0, 0, 0};
 	
-void Ticks(){
-	switch(state){
-		case START: 
-			state = INIT;
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
+void Tick() {
+	switch(state) {
+		case Start:
+			state = Init;
+			break;
+		case Init:
+			state = Lock;
+			break;
+		case Lock:
+			if (PINA == 0x04) {
+				arr[0] = 4;
+				state = Press;
+			}
+			else {
+				state = Lock;
+			}
+			break;
+		case Press:
+			if (PINA == 0x04 && arr[0] == 0x04) {
+				state = Press;
+			}
+			else if((PINA == 0x01) && (arr[0] == 0x04) && (arr[1] == 0) && arr[2] == 0 && arr[3] == 0) {
+				arr[1] = 0x01;
+				state = Press;
+			}
+			else if((PINA == 0x02) && (arr[0] == 0x04) && (arr[1] == 0x01) && arr[2] == 0 && arr[3] == 0) {
+				arr[2] = 0x02;
+				state = Press;
+			}
+			else if((PINA == 0x01) && (arr[0] == 0x04) && (arr[1] == 0x01) && (arr[2] == 0x02) && arr[3] == 0) {
+				arr[3] = 0x01;
+				state = Unlock;
+			}
+			else {
+				arr[0] = 0;
+				arr[1] = 0;
+				arr[2] = 0;
+				arr[3] = 0;
+				state = Lock;
+			}
+			break;
+		case Unlock:
+			if(PINA == 0x80) {
+				state = Lock;
+			}	
+			else if (PINA == 0x04) {
+				arr2[0] = 4;
+				state = Unlock_code;
+			}		
+			else {
+				state = Unlock;
+			}
+			break;
+		case Unlock_code:
+			if (PINA == 0x04 && arr2[0] == 0x04) {
+				state = Unlock_code;
+			}
+			else if((PINA == 0x01) && (arr2[0] == 0x04) && (arr2[1] == 0) && arr2[2] == 0 && arr2[3] == 0) {
+				arr2[1] = 0x01;
+				state = Unlock_code;
+			}
+			else if((PINA == 0x02) && (arr2[0] == 0x04) && (arr2[1] == 0x01) && arr2[2] == 0 && arr2[3] == 0) {
+				arr2[2] = 0x02;
+				state = Unlock_code;
+			}
+			else if((PINA == 0x01) && (arr2[0] == 0x04) && (arr2[1] == 0x01) && (arr2[2] == 0x02) && arr2[3] == 0) {
+				arr2[3] = 0x01;
+				state = Lock;
+			}
+			else {
+				arr2[0] = 0;
+				arr2[1] = 0;
+				arr2[2] = 0;
+				arr2[3] = 0;
+				state = Unlock;
+			}
+			break;
+		default:
+			state = Start;
+			break;
+	}
+	switch(state) {
+		case Start:
 			PORTC = 1;
 			break;
-		case INIT:
-			state = LOCK;
+		case Init:
 			PORTC = 2;
 			break;
-		case LOCK:
-			if(arr[cnt] == PINA){
-				state = PRESS;
-				cnt = cnt + 1;
-				PORTC = 0x10;
-			}
-			else if(arr[cnt] == PINA){
-				state = PRESS;
-				cnt = cnt + 1;
-				PORTC = 0x30;
-			}
-			else if(arr[cnt] == PINA){
-				state = PRESS;
-				cnt = cnt + 1;
-				PORTC = 0x70;
-			}
-			else if(arr[cnt] == PINA){
-				state = UNLOCK;
-				cnt = 0;
-				PORTC = 0xF0;
-			}
-			else{
-				state = LOCK;
-				cnt = 0;
-				PORTC = 3;
-			}
-			PORTD = cnt;
-			break;
-		case PRESS:
-			state = LOCK;
-			break;
-		case UNLOCK:
-			if(PINA == 0x80){
-				state = LOCK;
-				cnt = 0;
-				PORTC = 3;
-				
-			}
-			else if(arr[cnt] == PINA){
-				state = PRESS_UNLOCK;
-				cnt = cnt + 1;
-				PORTC = 5;
-			}
-			else if(arr[cnt] == PINA){
-				state = PRESS_UNLOCK;
-				cnt = cnt + 1;
-				PORTC = 5;
-			}
-			else if(arr[cnt] == PINA){
-				state = PRESS_UNLOCK;
-				cnt = cnt + 1;
-				PORTC = 5;
-			}
-			else if(arr[cnt] == PINA){
-				state = PRESS_UNLOCK;
-				cnt = 0;
-				PORTC = 5;
-			}
-			else{
-				state = UNLOCK;
-				cnt = 0;
-				PORTC = 5;
-			}
-			break;
-			
-		case PRESS_UNLOCK:
-			state = UNLOCK;
-			break;
-		default:
-			state = LOCK;
+		case Lock:
+			PORTB = 0;
 			PORTC = 3;
 			break;
-	}
-	switch(state){
-		case START:
+		case Press:
+			PORTC = 4;
 			break;
-		case INIT:
-			PORTB = 0x00;
+		case Unlock:
+			PORTC = 5;
+			PORTB = 1;
 			break;
-		case LOCK: 
-			PORTB = 0x00;
-			break;
-		case PRESS:
-			break;
-		case UNLOCK:
-			PORTB = 0x01;
-			break;
-		case PRESS_UNLOCK:
-			PORTB = 0x01;
+		case Unlock_code:
+			PORTC = 6;
 			break;
 		default:
-			PORTB = 0x00;
+			PORTC = 1;
 			break;
 	}
 }
 
-int main(void)
-{
-	DDRA = 0x00; PORTA = 0xFF;
+int main(void) {
+	DDRA = 0x00; PINA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
-	DDRD = 0xFF; PORTD = 0x00;
-	
-	state = START;
-    while (1) {Ticks();}
+	state = Start;
+    while (1) 
+    {
+	Tick();	
+    }
 }
+
 
