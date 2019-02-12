b8fcffd82284494400eda3336203bf91ead7cec4
clega001 <clega001@ucr.edu>
2019-02-12 15:29:52 -0800

    worked on lab10_part1

---
+++ clegaspino_clo_lab10_part1/clegaspino_clo_lab10_part1/main.c
@@ -0,0 +1,156 @@
+/*
+ * Christian Legaspino & clega001@ucr.edu
+ * Carissa Lo & clo020@ucr.edu
+ * Lab Section: 23
+ * Assignment: Lab 10 Exercise 1
+ *
+ * I acknowledge all content contained herein, excluding template or example
+ * code, is my own original work.
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1=0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+enum BL_States{BL_Start, BL_Blink} BL_state;
+enum TL_States{TL_Start, TL_Blink} TL_state;
+	
+unsigned char blinkingLED = 0x08;
+unsigned char threeLED = 0x04;
+unsigned char hold = 0x00;
+
+void TickFct_BL(){
+	switch(BL_state){
+		case BL_Start:
+			BL_state = BL_Blink;
+			break;
+		case BL_Blink:
+			BL_state = BL_Blink;
+			break;
+		default:
+			BL_state = BL_Start;
+			break;
+	}
+	switch(BL_state){
+		case BL_Start:
+			blinkingLED = 0x08;
+			break;
+		case BL_Blink:
+			if(blinkingLED == 0x08){
+				blinkingLED = 0x01;
+				break;
+			}
+			else if(blinkingLED == 0x01){
+				blinkingLED = 0x08;
+				break;
+			}
+			else{
+				break;
+			}
+		default:
+			break;
+	}
+}
+void TickFct_TL(){
+	switch(TL_state){
+		case TL_Start:
+			TL_state = TL_Blink;
+			break;
+		case TL_Blink:
+			TL_state = TL_Blink;
+			break;
+		default:
+			TL_state = TL_Start;
+			break;
+	}
+	switch(TL_state){
+		case TL_Start:
+			threeLED = 0x04;
+			break;
+		case TL_Blink:
+			if(threeLED == 0x04){
+				threeLED = 0x01;
+				break;
+			}
+			else if(threeLED == 0x01){
+				threeLED = 0x02;
+				break;
+			}
+			else if(threeLED == 0x02){
+				threeLED = 0x04;
+				break;
+			}
+			else{
+				break;
+			}
+		default:
+			break;
+	}
+}
+
+
+int main(void)
+{
+   DDRB = 0xFF; PORTB = 0x00;
+   
+   unsigned long BL_elapsedTime = 0;
+   unsigned long TL_elapsedTime = 0;
+   const unsigned long timerPeriod = 100;
+   
+   PORTB = 0x00;
+   TimerSet(timerPeriod);
+   TimerOn();
+   
+   BL_state = BL_Start;
+   TL_state = TL_Start;
+   PORTB = blinkingLED & threeLED;
+   
+   while(1){
+	   if(BL_elapsedTime >= 1000){
+		   TickFct_BL();
+		   BL_elapsedTime = 0;
+	   }
+	   if(TL_elapsedTime >= 1000){
+		   TickFct_TL();
+		   TL_elapsedTime = 0;
+	   }
+	   PORTB = blinkingLED | threeLED;
+	   while(!TimerFlag){}
+	   TimerFlag = 0;
+	   BL_elapsedTime += timerPeriod;
+	   TL_elapsedTime += timerPeriod;
+   }
+}
+
--- clegaspino_clo_lab7_part2/clegaspino_clo_lab7_part2/main.c
+++ clegaspino_clo_lab7_part2/clegaspino_clo_lab7_part2/main.c
@@ -13,136 +13,187 @@
 #include <avr/interrupt.h>
 #include "io.c"
 
+enum States{Start, Init, Blink1Wait, Blink2, Blink2Wait, Blink3, Blink3Wait, CheckScore} state;
 
-volatile unsigned char TimerFlag = 0; 
-unsigned char tmp = 0x00;
-unsigned char holder = 0x00;
-unsigned char b0 = 0x00;
-unsigned char val = 0x00;
-unsigned char cnt = 0x00;
+unsigned char button;
+unsigned char value = 5;
 
-enum States{INIT, S1, PRESS, PRESS_RELEASE} state;
+void Tick(){
+	button = (~PINA) & 0x01;
+	switch(state){
+		case Start:
+		LCD_Cursor(1);
+		LCD_WriteData( value + '0' );
+		state = Init;
+		break;
+		case Init:
+		LCD_Cursor(1);
+		LCD_WriteData( value + '0' );
+		if ((button) == 0x01){
+			state = Blink1Wait;
+		}
+		else{ state = Blink2; }
+		break;
+		case Blink1Wait:
+		if((button) == 0x00){
+			value--;
+			LCD_Cursor(1);
+			LCD_WriteData( value + '0' );
+			state = Init;
+		}
+		else{
+			state = Blink1Wait;
+		}
+		break;
+		case Blink2:
+		LCD_Cursor(1);
+		LCD_WriteData( value + '0' );
+		if ((button) == 0x01){
+			state = Blink2Wait;
+		}
+		else {state = Blink3;}
+		break;
+		case Blink2Wait:
+		if((button) == 0x00){
+			value++;
+			LCD_Cursor(1);
+			LCD_WriteData( value + '0' );
+			state = CheckScore;
+		}
+		else{
+			state = Blink2Wait;
+		}
+		break;
+		case Blink3:
+		LCD_Cursor(1);
+		LCD_WriteData( value + '0' );
+		if ((button) == 0x01){
+			state = Blink3Wait;
+		}
+		else {state = Init;}
+		break;
+		case Blink3Wait:
+		if((button) == 0x00){
+			value--;
+			LCD_Cursor(1);
+			LCD_WriteData( value + '0' );
+			state = Blink3;
+		}
+		else{
+			state = Blink3Wait;
+		}
+		break;
+		case CheckScore:
+		if(value == 9){
+			LCD_DisplayString(1, "WINNER");
+			state = CheckScore;
+		}
+		else{state = Blink2;}
+		break;
+		default:
+		state = Start;
+		break;
+	}
+	switch(state){
+		case Start:
+		break;
+		case Init:
+		PORTB = 0x01;
+		break;
+		case Blink1Wait:
+		PORTB = 0x01;
+		break;
+		case Blink2:
+		PORTB = 0x02;
+		break;
+		case Blink2Wait:
+		PORTB = 0x02;
+		break;
+		case Blink3:
+		PORTB = 0x04;
+		break;
+		case Blink3Wait:
+		PORTB = 0x04;
+		break;
+		case CheckScore:
+		break;
+		default:
+		break;
+	}
+}
 
-unsigned long _avr_timer_M = 1; 
-unsigned long _avr_timer_cntcurr = 0; 
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
 
 void TimerOn() {
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
+	// AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: pre-scaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A = 125;    // Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
 	TCNT1=0;
+
 	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; // 0x80: 1000000
 }
 
 void TimerOff() {
-	TCCR1B = 0x00; 
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
 }
 
 void TimerISR() {
 	TimerFlag = 1;
 }
 
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
 ISR(TIMER1_COMPA_vect) {
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0) { 
-		TimerISR(); 
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); // Call the ISR that the user uses
 		_avr_timer_cntcurr = _avr_timer_M;
 	}
 }
-void TimerSet(unsigned long M) {
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-void Tick(){
-	
-	b0 = PINA & 0x01;
-	
-	switch(state){
-		case INIT:
-		state = S1;
-		break;
-		case S1:
-		if(b0 == 0){
-			state = PRESS;
-		}
-		else{
-			state = S1;
-		}
-		break;
-		case PRESS:
-		if(b0 == 0){
-			state = PRESS;
-		}
-		else{
-			state = PRESS_RELEASE;
-		}
-		break;
-		case PRESS_RELEASE:
-		if(b0 == 0){
-			state = INIT;
-		}
-		else{
-			state = PRESS_RELEASE;
-		}
-		break;
-		default:
-		state = INIT;
-		break;
-	}
-	switch(state){
-		case INIT:
-			PORTB = 0x01;
-			val = 5;
-			LCD_Cursor(1);
-			LCD_WriteData(val + '0');
-			break;
-		case S1:
-			if(PORTB >= 7){
-				PORTB = 0x00;
-			}
-			else if(PORTB == 0x00){
-				PORTB = PORTB + 1;
-			}
-			else if(PORTB == 0x04){
-				PORTB = 0x01;
-			}
-			else {
-				tmp = PORTB << 1;
-				PORTB = tmp;
-			}
-			holder = PORTB;
-			LCD_DisplayString(1, "Winner!");
-			break;
-		case PRESS:
-			break;
-		case PRESS_RELEASE:
-			PORTB = holder;
-			cnt++;
-			break;
-		default:
-			PORTB = 0x01;
-			break;
-	}
-}
 
-int main()
+int main(void)
 {
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
-	DDRC = 0xFF; PORTC = 0x00;
-	
+
 	LCD_init();
-	TimerSet(100);
+	TimerSet(500);
 	TimerOn();
 	
-	
-	state = INIT;
-	while(1) {
-		
+	state = Start;
+
+	while (1)
+	{
 		Tick();
-		while (!TimerFlag){};
+		while(!TimerFlag){}
 		TimerFlag = 0;
+		continue;
 	}
 }
 
